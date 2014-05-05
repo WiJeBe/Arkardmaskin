bf5fa915efffb83849f316ac86bc35f677c9e100
Xtreeem - Hugo <Hugo.tinglof@gmail.com>
2014-05-05 21:51:50 +0200

    Testing Merging Tool

--- ProjectJumpyTHing/ProjectJumpyThing/GameObject.cs
+++ ProjectJumpyTHing/ProjectJumpyThing/GameObject.cs
@@ -8,5 +8,43 @@ namespace ProjectJumpyThing.gameobjects
     class GameObject
     {
         //test again
+        //Testing
+        //Testing more 
+        //Guess what im still testign
+         //Binary is beutiful
+
+        //01101000
+        //01100001
+        //01101000
+        //01100001
+        //00100000
+        //01101101
+        //01100001
+        //01100100
+        //01100101
+        //00100000
+        //01111001
+        //01101111
+        //01110101
+        //00100000
+        //01101100
+        //01101111
+        //01101111
+        //01101011
+
+        //Is is not?
+
+        int dummy;
+
+        public GameObject()
+        {
+            dummy = 2;
+        }
+
+        public int DummyFunction(int test)
+        {
+            return test * dummy;
+        }
+
     }
 }
