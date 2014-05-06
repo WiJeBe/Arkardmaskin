a4dbf846e5cd42f5cb478468610aafaeac00614a
Xtreeem - Hugo <Hugo.tinglof@gmail.com>
2014-05-06 15:06:43 +0200

    Working on Manager  and implemented empty classes for platform, player and powerups

--- ProjectJumpyTHing/ProjectJumpyThing/Manager.cs
+++ ProjectJumpyTHing/ProjectJumpyThing/Manager.cs
@@ -7,5 +7,43 @@ namespace ProjectJumpyThing
 {
     class Manager
     {
+        #region Variables
+        #endregion
+
+        #region Public Methods
+        #endregion
+
+        #region Private Methods
+        #endregion
+
+        private void CollisionChecking(MovableGameObject ObjectA, MovableGameObject ObjectB)
+        {
+            if (ObjectA is Player)
+            {
+                if (ObjectB is Platform && ObjectA.velocity.Y > 0)
+                {
+                    (ObjectB as Platform);
+                    if (
+                        (ObjectB as Platform).SurfaceRectangle.Contains(ObjectA.rectangle.Left, ObjectA.rectangle.Bottom) ||
+                        (ObjectB as Platform).SurfaceRectangle.Contains(ObjectA.rectangle.Center, ObjectA.rectangle.Bottom) ||
+                        (ObjectB as Platform).SurfaceRectangle.Contains(ObjectA.rectangle.Right, ObjectA.rectangle.Bottom)
+                        )
+                    {
+                        ObjectA.velocity.Y = 0;
+                        ObjectA.position.X = (ObjectB as Platform).SurfaceRectangle.Top + 1;
+                    }
+                }
+                else if (ObjectB is Player)
+                {
+                    
+                }
+
+                else if (ObjectB is PowerUp)
+                {
+
+                }
+            }
+        }
     }
 }
+
--- ProjectJumpyTHing/ProjectJumpyThing/Platform.cs
+++ ProjectJumpyTHing/ProjectJumpyThing/Platform.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
 namespace ProjectJumpyThing
 {
-    class Platform
+    class Platform : MovableGameObject
     {
+        public Rectangle SurfaceRectangle;
     }
 }
