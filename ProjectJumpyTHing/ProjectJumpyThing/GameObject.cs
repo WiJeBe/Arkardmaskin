8632eb786e4f8ce69200b12bc5a59c4ebf4a8453
azkaz <philip.sjoholm@gmail.com>
2014-05-06 09:37:31 +0200

    added variables in GameObject

--- ProjectJumpyTHing/ProjectJumpyThing/GameObject.cs
+++ ProjectJumpyTHing/ProjectJumpyThing/GameObject.cs
@@ -1,53 +1,30 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace ProjectJumpyThing.gameobjects
 {
-    class GameObject
+
+    abstract class GameObject
     {
-
-        //Laptop says hello
-        //test again
-        //Testing
-        //merger Test
-        //Testing more 
-        //Guess what im still testign
-         //Binary is beutiful
-
-        //01101000
-        //01100001
-        //01101000
-        //01100001
-        //00100000
-        //01101101
-        //01100001
-        //01100100
-        //01100101
-        //00100000
-        //01111001
-        //01101111
-        //01110101
-        //00100000
-        //01101100
-        //01101111
-        //01101111
-        //01101011
-
-        //Is is not?
-
-        int dummy;
-
-        public GameObject()
+        public Texture2D texture;
+        public Vector2 position;
+        public Vector2 origin;
+        public bool isDead;
+        public SpriteEffects spriteEffect;
+        public double scale;
+        public Color color; 
+        public abstract GameObject()
         {
-            dummy = 2;
+
         }
 
-        public int DummyFunction(int test)
+        public abstract void Draw(SpriteBatch spritebatch)
         {
-            return test * dummy;
-        }
 
+        }
     }
 }
