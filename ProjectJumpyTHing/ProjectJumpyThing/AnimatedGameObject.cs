6ccf5b65bd78ab4376ba94fb643640aa7f05fad5
azkaz <philip.sjoholm@gmail.com>
2014-05-08 13:48:45 +0200

    misc

---
+++ ProjectJumpyTHing/ProjectJumpyThing/AnimatedGameObject.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectJumpyThing
+{
+    class AnimatedGameObject : MovableGameObject
+    {
+        public int numFrames;
+        public Rectangle drawRextangle;
+        public Rectangle sourceRectangle;
+        public int currentFrame;
+
+        public void Animate()
+        {
+
+        }
+    }
+}
